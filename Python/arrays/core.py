baa2a58c3cabcc75eec47fd1acd0eaefe1e47d53
Daniel Zanotelli <danielzanotelli12@gmail.com>
2026-02-14 12:38:16 -0300

    feat: implementa array `core` para simular um array de baixo nivel em C

---
+++ Python/arrays/core.py
@@ -0,0 +1,26 @@
+import array
+from typing import Union
+
+class Array:
+    def __init__(self, size: int, typecode: str = 'l'):
+        if size <= 0:
+            raise ValueError(f'Invalid array size (must be positive): {size}')
+        self._size = size
+        self._array = array.array(typecode, [0] * size)
+    
+    def __len__(self):
+        return self._size
+    
+    def __getitem__(self, index: int)-> Union[int, float]:
+        if index < 0 or index >= self._size:
+            raise IndexError('array index out of range')
+        return self._array[index]
+    
+    def __setitem__(self, index: int, value: Union[int,float]) -> None:
+        if index < 0 or index >= self._size:
+            raise IndexError('array index out of range')
+        self._array[index] = value
+        
+    def __repr__(self):
+        return repr(self._array)
+    
